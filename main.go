5f9dd0ce2e6cd7d72ddcf710b537fc718edf0fe5
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2015-05-29 20:12:20 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package fanvil;
+
+type Fanvil {
+	addr string;
+}
+
+func New(addr string) (p *Fanvil) {
+	return Fanvil{addr: addr};
+}
+
+func (p *Fanvil) SetCfgUrl(cfgurl string) (error) {
+	return nil;
+}
+
+func (p *Fanvil) Reboot( string) (error) {
+	return nil;
+}
+
